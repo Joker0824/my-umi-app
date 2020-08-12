e5f6ed17d773c510f4c77fd60967fdbcabb6ba09
Joker0824 <314231258@qq.com>
2020-08-12 16:18:19 +0800

    feat(request): 去除 空字符串,null,undefined

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,12 +1,22 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 import { getToken, API_KEY } from '@/utils/auth';
 import { message } from 'antd';
+import _ from 'lodash';
+import Qs from 'qs';
 import { apiConfig } from '@/api/config';
 const { AXIOS_TIMEOUT, ApiUrl } = apiConfig;
 const service = axios.create({
   baseURL: ApiUrl, // url = base url + request url
   timeout: AXIOS_TIMEOUT,
   withCredentials: true, // send cookies when cross-domain requests
+  paramsSerializer(params) {
+    // 去除 空字符串,null,undefined
+    let formatParams = _.omitBy(params, value =>
+      [null, '', undefined].includes(value),
+    );
+    let res = Qs.stringify(formatParams, { arrayFormat: 'brackets' });
+    return res;
+  },
 });
 
 // 请求拦截器
