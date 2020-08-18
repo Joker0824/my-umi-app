2fbdfb22225d06634905f061548c82080b343c38
Joker0824 <314231258@qq.com>
2020-08-18 15:48:19 +0800

    perf(登录接口): 去掉async

--- src/api/login.ts
+++ src/api/login.ts
@@ -3,7 +3,7 @@ export /**
  * @description 登录
  * @param {ILoginForm} params
  */
-const login = async (params: ILoginParams) =>
+const login = (params: ILoginParams) =>
   request({
     url: '/user/login',
     method: 'post',
