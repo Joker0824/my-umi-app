3632fc7b3614c9a88c946e0c5062e9ebec707b6d
Joker0824 <314231258@qq.com>
2020-08-18 17:58:33 +0800

    perf(config.ts): config.ts中直接定义routes,否则使用财产提示无法找到路由配置数组

--- config/config.ts
+++ config/config.ts
@@ -8,7 +8,21 @@ export default defineConfig({
   },
   hash: true, //配置是否让生成的文件包含 hash 后缀，通常用于增量发布和避免浏览器加载缓存。
   dynamicImport: {}, //启用按需加载
-  routes: routes,
+  routes: [
+    { path: '/login', component: '@/pages/login' },
+    {
+      path: '/',
+      component: '@/layouts',
+      routes: [
+        { path: '/list/:id', component: '@/pages/layouts/list' },
+        {
+          path: '/admin',
+          component: '@/pages/layouts/admin',
+          wrappers: ['@/wrappers/auth'],
+        },
+      ],
+    },
+  ],
   extraBabelPlugins: [
     [
       'import',
