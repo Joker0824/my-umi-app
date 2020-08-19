c81abaf105eabbf5fad026fc9a123c1519be506b
Joker0824 <314231258@qq.com>
2020-08-19 16:18:58 +0800

    feat(page): 添加拓扑图编辑器

--- config/config.ts
+++ config/config.ts
@@ -5,19 +5,32 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  hash: true, //配置是否让生成的文件包含 hash 后缀，通常用于增量发布和避免浏览器加载缓存。
-  dynamicImport: {}, //启用按需加载
+  hash: true,
+  //配置是否让生成的文件包含 hash 后缀，通常用于增量发布和避免浏览器加载缓存。
+  dynamicImport: {},
+  //启用按需加载
   routes: [
-    { path: '/login', component: '@/pages/login' },
+    {
+      path: '/login',
+      component: '@/pages/login',
+    },
     {
       path: '/',
       component: '@/layouts',
       routes: [
-        { path: '/list/:id', component: '@/pages/layouts/list' },
+        {
+          path: '/list/:id',
+          component: '@/pages/layouts/list',
+        },
         {
           path: '/admin',
           component: '@/pages/layouts/admin',
           wrappers: ['@/wrappers/auth'],
+        },
+        {
+          name: '拓扑编辑器',
+          path: '/editorkoni',
+          component: './EditorKoni',
         },
       ],
     },
